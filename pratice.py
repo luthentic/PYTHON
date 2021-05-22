43b9de626624a4c0a8837056232fbde979bfed17
Lee <dltpdnd1234@gmail.com>
2021-05-21 22:31:23 -0500

    PY by for loop

---
+++ pratice.py
@@ -0,0 +1,33 @@
+# 5 / 5 float 
+# 5 // 5 integer
+# 5 ** 5 integer
+
+# greet = 'hello' string
+# greet[0] = h
+# greet[0:3] = hell
+
+# name = input('Tell me your name:')
+# name = 3 * 9 * int(name) **2
+# print(name)
+
+# num1 = 3.19495959
+# num2 = 10.51204024
+# format method
+# print('num 1 is {0:.3} and num2 is {1:.4}'.format(num1,num2))
+
+# age =   int(input('Enter your ages:'))
+
+# if age<20:
+#   print('you are under 20')
+# elif age == 30:
+#   print('you are under 30') 
+# else:
+#   print('you are wise')
+
+LEE = ['SW','SB','SM','SY']
+for LEEE in LEE:
+  if LEEE == 'SW':
+    print(LEEE)
+    break
+  else: print(LEE)
+  
