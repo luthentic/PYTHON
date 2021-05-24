120672ef4e61d0c275151ad7cc900ee0057bb182
Lee <dltpdnd1234@gmail.com>
2021-05-24 01:06:08 -0500

    PY class

--- pratice.py
+++ pratice.py
@@ -101,22 +101,51 @@
 # print(belt.values())
 # belt['5'] = 'green'
 # print(belt)
-def loop(dic):
-  for key,value in dic.items():
-    print(f'I am{key} and I am a {value} belt')
 
-belts = {}
+# def loop(dic):
+#   belts = list(dic.values())
+#   for belt in set(belts):
+#     num = belts.count(belt)
+#     print(f'There are{num},{belt}belts')
 
-while True:
-  name = input('enter name:')
-  color = input('enter color:')
-  belts[name] = color
+# belts = {}
 
-  check = input('Countinue? (y/n)')
-  if check == 'y':
-    continue
-  else:
-    break
+# while True:
+#   name = input('enter name:')
+#   color = input('enter color:')
+#   belts[name] = color
 
-loop(belts)
+#   check = input('Countinue? (y/n)')
+#   if check == 'y':
+#     continue
+#   else:
+#     break
+
+# loop(belts)
 #----------------------------------------------------------------------------------------------------------------
+
+#CLASS
+class Planet:
+  shape = 'round'
+
+  def __init__(self,name,radius,gravity,system):
+    self.name = name
+    self.radius = radius
+    self.gravity = gravity
+    self.system = system
+  def orbit(self):
+    return f'{self.name} is orbiting in the {self.system}'
+  @classmethod
+  def commons(cls):
+    return f'All planets are {cls.shape}'
+  @staticmethod
+  def spin(speed='2000 mile per hour'):
+    return f'The speed and spins at {speed}'
+
+hoth = Planet('LEE',20000,5.6,'HOHOHO')
+print(f'Name is:{hoth.name}')
+print(f'Radius is:{hoth.radius}')
+print(f'Gravity is:{hoth.gravity}')
+print(hoth.orbit())
+print(Planet.commons())
+print(Planet.spin('1000 miles'))
